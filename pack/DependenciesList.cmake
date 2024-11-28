d1edc3a75c79ae7586036c5c38472eceee29d9c3
Abdulkareem Siddiq <aa@siddiq.org>
2024-11-28 15:54:46 -0800

    updated dependencies

--- pack/DependenciesList.cmake
+++ pack/DependenciesList.cmake
@@ -12,5 +12,19 @@ include(pack/CPM.cmake)
 function(DependenciesList)
     # ________________________
     # our project dependencies
-    CPMAddPackage("gh:nlohmann/json@3.11.3")
+    #cpmaddpackage("gh:nlohmann/json#9cca280a4d0ccf0c08f47a99aa71d1b0e52f8d03")# v3.11.3
+    CPMAddPackage(  NAME nlohmann_json
+                    VERSION 3.11.3
+                    GITHUB_REPOSITORY nlohmann/json
+                    OPTIONS "JSON_BuildTests OFF")
+    # This is the fastest download; avoid using the shortcut since this repo is huge and we'd end up
+    # downloading a ton of history for this project just to link to it.
+    #CPMAddPackage(  NAME nlohmann_json
+    #                VERSION 3.11.3
+    #                URL https://github.com/nlohmann/json/releases/download/v3.11.3/include.zip)
+    
+    #if(nlohmann_json_ADDED)	 
+    #    add_library(nlohmann_json INTERFACE)	 
+    #    target_include_directories(nlohmann_json INTERFACE ${nlohmann_json_SOURCE_DIR})	 
+    #endif()
 endfunction()
