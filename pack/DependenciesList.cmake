1112eed5bfff8fbeca28eef5b0400e529ae8f3e4
abdulkareem-siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2024-11-28 16:32:55 -0800

    Feature/cmake barebones 1 (#22)
    * Added the raw cmake support
    
    * Update nuget to v6.x
    
    * Updated the dependency and the presets to always build the tests and package in Linux Release mode
    
    * Tweaked the coverage collection mask
    
    * Install the nuget tool in the publish stage
    
    * Fixed the nuspec to include the correct path for the source header files.
    
    * Cleanup to the pipelines file
    
    * Updated the cmake for clarity
    
    * Updated to add build as source package support.
    
    * Updated files
    
    * Updated to build the src zip in azure.
    
    * Updated the pipelines to remove cruft
    
    * Cleaned up the build and the support files
    
    * Switch output of coverage to jacoco
    
    * Remove old packing support
    
    * Fixed up the dependencies calling convention.
    
    * updated dependencies
    
    * Tweaked the pipelines for coverage
    
    * Update to simplify the coverage data
    
    ---------
    
    Co-authored-by: Abdulkareem Siddiq <aa@siddiq.org>

---
+++ pack/DependenciesList.cmake
@@ -0,0 +1,27 @@
+#
+# Dependencies List
+# https://github.com/cpm-cmake/CPM.cmake
+#
+
+# download CPM.cmake..
+file(DOWNLOAD https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake ${CMAKE_CURRENT_SOURCE_DIR}/pack/CPM.cmake)
+# import the helper into our process..
+include(pack/CPM.cmake)
+
+# Wrap the depdendency list in a function to prevent options from leaking to the parent
+function(DependenciesList)
+    # ________________________
+    # our project dependencies
+    cpmaddpackage("gh:nlohmann/json#9cca280a4d0ccf0c08f47a99aa71d1b0e52f8d03")# v3.11.3
+
+    # This is the fastest download; avoid using the shortcut since this repo is huge and we'd end up
+    # downloading a ton of history for this project just to link to it.
+    #CPMAddPackage(  NAME nlohmann_json
+    #                VERSION 3.11.3
+    #                URL https://github.com/nlohmann/json/releases/download/v3.11.3/include.zip)
+    
+    #if(nlohmann_json_ADDED)	 
+    #    add_library(nlohmann_json INTERFACE)	 
+    #    target_include_directories(nlohmann_json INTERFACE ${nlohmann_json_SOURCE_DIR})	 
+    #endif()
+endfunction()
